031b431d548671a821adba315fddc4892667171b
Lucas <lucasspindolafernandes196@hotmail.com>
2023-03-21 15:37:39 -0300

    start-branch-develop

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import { StyledHeader } from './components/Header/style';
+function App() {
+  return (
+    <div className="App">
+    <StyledHeader>HELLO</StyledHeader>
+    </div>
+  );
+}
+
+export default App;
